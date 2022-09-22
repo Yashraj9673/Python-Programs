21b41c0ec3d1f9bc1c0b45154b0259455d17937d
Yashraj9673 <yashrajchaudhari48924@gmail.com>
2022-09-22 18:42:19 +0530

    initial commit

---
+++ 02_Percen_txt.py
@@ -0,0 +1,30 @@
+from typing import Text
+
+b = input("Enter your name : ")
+a = int(input("Enter your Percentage: "))
+
+if (a<35):
+    print("You are Fail!")
+
+elif(a>35 and a<=50):
+    print("You are passed!")
+
+elif(a>50 and a<=80):
+    print("You are Passed with First class!")
+
+elif(a>80 and a<=100):
+    print("You are passed with Destination!")
+
+else:
+    print("Enter valid Percentage!")
+
+# file save with txt format
+
+with open("Program/Text.txt", "r") as f :
+    Text = f.read()
+with open("Program/Text.txt", "a") as f :
+    f.write(str("\n"+ b +":"))
+    f.write(str(str(a)))
+
+
+
