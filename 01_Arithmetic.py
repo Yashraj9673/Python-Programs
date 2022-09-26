fd9183295fc36bc2677c35f0cd2a6ab8895f8915
Yashraj9673 <yashrajchaudhari48924@gmail.com>
2022-09-26 18:56:33 +0530

    initial commit

--- 01_Arithmetic.py
+++ 01_Arithmetic.py
@@ -1,11 +1,13 @@
 a = int(input("Enter First number: "))
 b = int(input("Enter Second number: "))
 
-print("\nAddition of a and b is: ", a + b)
+print("\nAddition of a and b is: ", a - b)
 
-print("substraction of a and b is: ",a - b)
+print("substraction of a and b is: ",a + b)
 
 print("Multiplication of a and b is: ",a * b)
 
 print("Division of a and b is:",a % b)
+
+
 
